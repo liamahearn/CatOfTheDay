d5d131650fe371741c873009157ce02b9711bfe7
Liam A'Hearn <87583676+liamahearn@users.noreply.github.com>
2024-11-16 13:46:48 -0500

    Cat of the Day Initial Commit
    This is what I developed almost entirely on a flight with very little resources

---
+++ scripts/catOfTheDay.js
@@ -0,0 +1,15 @@
+let entries = [
+    {
+        picture: "cat1.png",
+        emoji: "😸",
+        source: "miguelitolopez2002",
+        background: "tilingBG1Medium.png",
+        backgroundAnimation: "vertical-scroll"
+    },
+    {
+        
+
+
+
+    }
+]
